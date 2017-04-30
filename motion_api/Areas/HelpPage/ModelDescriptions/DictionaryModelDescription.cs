96241efbdf9ef4abce5240647814a0fa15f6d37e
yaman alsaadi jbawi <yaman alsaadi jbawi>
2017-04-29 20:20:58 -0700

    first work

---
+++ motion_api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace motion_api.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ motion_api/Controllers/ValuesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace motion_api.Controllers
+{
+    public class ValuesController : ApiController
+    {
+
+       static List<String> strings = new List<string>(){"Yaman","Tareef"};
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return strings;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return strings.ElementAtOrDefault(id);
+        }
+        [HttpPost]
+        public HttpResponseMessage Post()
+        {
+            var httpRequest = HttpContext.Current.Request;
+            if (httpRequest.Files.Count < 1)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            foreach (string file in httpRequest.Files)
+            {
+                var postedFile = httpRequest.Files[file];
+                var filePath = HttpContext.Current.Server.MapPath("~/App_Data/" + postedFile.FileName);
+                postedFile.SaveAs(filePath);
+                // NOTE: To store in memory use postedFile.InputStream
+            }
+
+            return Request.CreateResponse(HttpStatusCode.Created);
+        }
+
+        // POST api/values
+        
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
